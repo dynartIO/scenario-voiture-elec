9b397361f8cfeb6f6a738173a5d1d71ba930204e
RIBON BENOIT <benoit.ribon@live-cnrs.unistra.fr>
2022-10-27 18:56:24 +0200

    Version pre-1.0b

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2022 - Benoit Ribon (dynartI/O) - https://dynartio.com
+Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le modifier suivant les termes de la GNU General Public License telle que publiée par la Free Software Foundation ; soit la version 3 de la licence, soit (à votre gré) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie tacite de QUALITÉ MARCHANDE ou d'ADÉQUATION à UN BUT PARTICULIER. Consultez la GNU General Public License pour plus de détails.
+Vous devez avoir reçu une copie de la GNU General Public License en même temps que ce programme ; si ce n'est pas le cas, consultez <http://www.gnu.org/licenses>. 
+*/
 
 var initvalues = {
 "nb_vehicules":32, // millions 
@@ -5,7 +11,7 @@ var initvalues = {
 "km_annuel":12200, // km annuels 
 "ges_structure_vt":6, // t eqCO2  
 "ges_structure_ve":6, // t eqCO2 
-"conso_vt":6, // L/100km 
+"conso_vt":6.5, // L/100km 
 "conso_ve":15, // kWh/100km 
 "ges_reseau_ve":59.9, //g eq CO2/kWh 
 "ges_reseau_vt":2.75, // kg eq CO2/L 
@@ -13,7 +19,6 @@ var initvalues = {
 "duree_vie_ve":20, //ans 
 "autonomie_ve":400, //km 
 "ges_batterie_ve":75, // kg eq CO2/kWh
-
 }
 
 var barwidth = 40;
@@ -24,8 +29,8 @@ var calcvalues = {};
 var listfields = Object.keys(initvalues);
 var svg = d3.select("#dataviz_area")
 
-svgheight = 250;
-svgscale = 2; // px par millions de tonnes CO2
+var svgheight = 250;
+var svgscale = 2; // px par millions de tonnes CO2
 var myval;
 var xpos = barspace+10;
 
@@ -191,15 +196,8 @@ function draw_bar(start, height, xlabel, color, posval, showbar) {
 			.attr("y1", svgheight-start*svgscale)
 			.attr("x2", xpos+barwidth)
 			.attr("y2", svgheight-start*svgscale);
-	} else {
-		/*svg.append('line')
-			.style("stroke", "black")
-			.style("stroke-width", 1)
-			.attr("x1", xpos-barwidth + barspace)
-			.attr("y1", svgheight-start*svgscale)
-			.attr("x2", xpos+barwidth)
-			.attr("y2", svgheight-start*svgscale);*/
-	}
+	} 
+	
 	xpos += barwidth + barspace;
 }
 	
@@ -256,11 +254,9 @@ function update(event) {
 	for (const field of listfields) {
 	   uservalues[field] = parseFloat(document.getElementById(field).value);
 	}
-	//console.log("UPDATE");
-	
+
 	calcvalues = do_calc(uservalues);
-	//console.log(calcvalues);
-	// Infos as text
+
 	document.getElementById("pct_vt").innerHTML = Math.round(100 - uservalues["part_ve"]);
 	document.getElementById("pct_ve").innerHTML = Math.round(uservalues["part_ve"]);
 	
@@ -275,23 +271,19 @@ function update(event) {
 	document.getElementById("val_km_annuel").innerHTML = uservalues["km_annuel"];
 	
 	document.getElementById("division_scenario").innerHTML = Math.round(initcalcvalues["bilan_total"]*10/calcvalues["bilan_total"])/10;
+	
 	reduction = Math.round((calcvalues["bilan_total"]-initcalcvalues["bilan_total"])*100/initcalcvalues["bilan_total"]);
 	myelement = document.getElementById("reduction_scenario");
 	if (reduction > 0) {
 		myelement.innerHTML = "+" + reduction + "&nbsp;%";
-		//myelement.parentElement.parentElement.className = "big_content spot_synthese_value";
 	} else if (reduction < 0) {
 		myelement.innerHTML = reduction + "&nbsp;%"
-		//myelement.parentElement.parentElement.className = "big_content spot_synthese_value";
 	} else {
-		//myelement.parentElement.parentElement.className = "spot_meme_qte spot_synthese_value";
 		myelement.innerHTML = "Même quantité";
 	}
 	
-	
 	synthese_scenario(initvalues, do_calc(initvalues), uservalues, calcvalues);
-	//console.log("Bilan total = " + Math.round(calcvalues["bilan_total"]*10)/10 + " millions de tonnes eq CO2");
-	
+
 	draw();
 }
 
